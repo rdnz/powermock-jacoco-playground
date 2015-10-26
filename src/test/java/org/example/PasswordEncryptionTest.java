9fa7027c1b9f2e73581035fbeaf60c7edbfc1795
unknown <rzeng@PL-77XMBS1.corp.securustech.net>
2015-10-26 11:10:34 -0500

    Successful without applicationContext.xml.

--- src/test/java/org/example/PasswordEncryptionTest.java
+++ src/test/java/org/example/PasswordEncryptionTest.java
@@ -3,8 +3,6 @@ package org.example;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
